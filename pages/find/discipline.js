756b69f69e38c19e69b811dedd95b704f3333790
Happy-Daddy <xujinggg@qq.com>
2018-03-20 09:33:34 +0800

    Add find disipline

--- pages/find/discipline.js
+++ pages/find/discipline.js
@@ -1,118 +1,42 @@
-import * as echarts from '../../ec-canvas/echarts';
-
-let chart = null;
-let chart2 = null;
-
-function initChart(canvas, width, height) {
-  chart = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(chart);
-
-  var option = {
-    xAxis: {
-      name:'(年)',
-      nameGap:4,
-      type: 'category',
-      boundaryGap: false,
-      data: ['2012', '2013', '2014', '2015', '2016', '2017', '2018']
-    },
-    yAxis: {
-      name:'(元/月)',
-      type: 'value'
-    },
-    series: [{
-      data: [4000, 6000, 7000, 8000, 8300, 8500, 9200],
-      type: 'line',
-      areaStyle: {}
-    }]
-  };
-
-  chart.setOption(option);
-  return chart;
-}
-
-
-function initChart2(canvas, width, height) {
-  chart2 = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(chart);
-
-  var option = {
-    tooltip: {
-      trigger: 'item',
-      formatter: "{a} <br/>{b}: {c} ({d}%)"
-    },
-    legend: {
-      // orient: 'horizontal',
-      // // x: 'center',
-      // left:'center',
-      data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-    },
-    series: [
-      {
-        name: '访问来源',
-        type: 'pie',
-        radius: ['50%', '70%'],
-        avoidLabelOverlap: false,
-        label: {
-          normal: {
-            show: false,
-            position: 'center'
-          },
-          emphasis: {
-            show: true,
-            textStyle: {
-              fontSize: '30',
-              fontWeight: 'bold'
-            }
-          }
-        },
-        labelLine: {
-          normal: {
-            show: true
-          }
-        },
-        data: [
-          { value: 335, name: '直接访问' },
-          { value: 310, name: '邮件营销' },
-          { value: 234, name: '联盟广告' },
-          { value: 135, name: '视频广告' },
-          { value: 1548, name: '搜索引擎' }
-        ]
-      }
-    ]
-  };
-
-  chart2.setOption(option);
-  return chart2;
-}
-
 Page({
-  onShareAppMessage: function (res) {
-    return {
-      title: 'ECharts 可以在微信小程序中使用啦！',
-      path: '/pages/index/index',
-      success: function () { },
-      fail: function () { }
-    }
-  },
   data: {
-    ec: {
-      onInit: initChart
-    },
-    ec2:{
-      onInit: initChart2
-    }
+    inputShowed: false,
+    inputVal: "",
+    tabSelected:0,
+    categorySelected:0,
+    contentShowed:false
   },
-
-  onReady() {
-    setTimeout(function () {
-      // 获取 chart 实例的方式
-      console.log(chart)
-    }, 2000);
+  tabClick: function (e) {
+    this.setData({
+      tabSelected: e.currentTarget.id
+    });
+  },
+  categoryClick: function (e) {
+    this.setData({
+      categorySelected: parseInt(e.currentTarget.id)
+    });
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true,
+      contentShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false,
+      contentShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   }
 });
